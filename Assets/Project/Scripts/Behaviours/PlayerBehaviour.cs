2adb461638897f0d6c351421c9118b19faabba59
Eugene Nalivkin <CoderLivingDeath@gmail.com>
2025-08-10 00:41:57 +0700

    implemented interaction
    - Implemeted interation
    - Update gameplay scene

--- Assets/Project/Scripts/Behaviours/PlayerBehaviour.cs
+++ Assets/Project/Scripts/Behaviours/PlayerBehaviour.cs
@@ -3,12 +3,18 @@ using GameJamLvl5.Project.Infrastructure.EventBus.Subscribers;
 using UnityEngine;
 using Zenject;
 
-[RequireComponent(typeof(MovementBehaviour))]
-public class PlayerBehaviour : MonoBehaviour, IGameplay_MovementEventHandler
+[RequireComponent(typeof(MovementBehaviour), typeof(InteractionBehaviour))]
+public class PlayerBehaviour : MonoBehaviour, IGameplay_MovementEventHandler, IGameplay_InteractEventHandler
 {
     [Inject] private EventBus _eventBus;
 
     private MovementBehaviour _movementBehaviour;
+    private InteractionBehaviour _interactionBehaviour;
+
+    public void HandleInteract(bool button)
+    {
+        _interactionBehaviour.Interact();
+    }
 
     public void HandleMovement(Vector2 direction)
     {
@@ -24,6 +30,7 @@ public class PlayerBehaviour : MonoBehaviour, IGameplay_MovementEventHandler
     private void Start()
     {
         _movementBehaviour = GetComponent<MovementBehaviour>();
+        _interactionBehaviour = GetComponent<InteractionBehaviour>();
     }
     #endregion
 }
