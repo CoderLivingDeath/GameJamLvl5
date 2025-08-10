fa7a6ff1dbf58ec4f5764917ef7be7280be3997e
Eugene Nalivkin <CoderLivingDeath@gmail.com>
2025-08-10 21:05:51 +0700

    update gameplay view

--- Assets/Project/UI/Scripts/Behaviours/GameplayView.cs
+++ Assets/Project/UI/Scripts/Behaviours/GameplayView.cs
@@ -7,21 +7,34 @@ using Cysharp.Threading.Tasks;
 
 public class GameplayView : ViewBehaviourBase
 {
-    // Reactive property representing whether the popup is currently shown
-    private readonly ReactiveProperty<bool> _isShown = new ReactiveProperty<bool>(false);
-
-    // Observable that emits true when popup is shown
-    public IObservable<bool> OnShow => _isShown.Where(isShown => isShown);
-
-    // Observable that emits true when popup is closed
-    public IObservable<bool> OnClose => _isShown.Where(isShown => !isShown);
 
     [SerializeField]
     private ViewBehaviourBase _settingsPopup;
+
+    [SerializeField]
+    private ViewBehaviourBase _journalPopup;
+
     [SerializeField]
     private Button _showSettingsPopupButton;
 
+    [SerializeField]
+    private Button _showJournalPopupButton;
+
     #region Commands
+
+
+    public ICommand ShowJournalPopupCommand => _ShowJournalPopupCommand;
+    private ICommand _ShowJournalPopupCommand;
+
+    private void OnShowJournalPopupCommand(object param)
+    {
+        _journalPopup.ShowAsync().Forget();
+    }
+
+    private bool CanShowJournalPopupCommand(object param)
+    {
+        return true;
+    }
     public ICommand ShowSettingPopupCommand => _ShowSettingPopupCommand;
     private ICommand _ShowSettingPopupCommand;
 
@@ -37,21 +50,18 @@ public class GameplayView : ViewBehaviourBase
     private void CommandsSetup()
     {
         _ShowSettingPopupCommand = new LambdaCommand(OnShowSettingPopupCommand, CanShowSettingPopupCommand);
+        _ShowJournalPopupCommand = new LambdaCommand(OnShowJournalPopupCommand, CanShowJournalPopupCommand);
     }
     #endregion
 
     public override async UniTask ShowAsync()
     {
-        _isShown.Value = true;
-
         gameObject.SetActive(true);
         await UniTask.CompletedTask;
     }
 
     public override async UniTask CloseAsync()
     {
-        _isShown.Value = false;
-
         gameObject.SetActive(false);
         await UniTask.CompletedTask;
     }
@@ -60,6 +70,7 @@ public class GameplayView : ViewBehaviourBase
     {
         CommandsSetup();
 
-        _showSettingsPopupButton.onClick.AddListener(() => _ShowSettingPopupCommand.Execute(null));
+        _showSettingsPopupButton.onClick.AddListener(() => ShowSettingPopupCommand.Execute(null));
+        _showJournalPopupButton.onClick.AddListener(() => ShowJournalPopupCommand.Execute(null));
     }
 }
